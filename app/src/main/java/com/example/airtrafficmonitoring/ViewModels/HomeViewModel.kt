790cc66d6df445268ed6f4bc34fc1db428cbfa2a
Jean Khoge <khogejean@gmail.com>
2023-10-20 16:02:33 +0200

    Request the api to get the list of flights

--- app/src/main/java/com/example/airtrafficmonitoring/ViewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/airtrafficmonitoring/ViewModels/HomeViewModel.kt
@@ -1,19 +1,83 @@
 package com.example.airtrafficmonitoring.ViewModels
 
+import android.icu.util.Calendar
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.airtrafficmonitoring.Airport
+import com.example.airtrafficmonitoring.RequestManager
+import com.example.airtrafficmonitoring.Utils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeViewModel : ViewModel() {
-    private val airportListData = MutableLiveData<List<Airport>>()
 
-    fun setAirportList(airportList: List<Airport>) {
-        airportListData.value = airportList
+    private val beginDateLiveData = MutableLiveData<Calendar>(Calendar.getInstance())
+    private val endDateLiveData = MutableLiveData<Calendar>(Calendar.getInstance())
+
+    private val airportListLiveData = MutableLiveData<List<Airport>>()
+    private val airportListNamesLiveData = MutableLiveData<List<String>>()
+
+    init{
+        val airportList = Utils.generateAirportList()
+        airportListLiveData.value = airportList
+        val airportNamesList = ArrayList<String>()
+        //Populate the list of airport names
+        for (airport in airportList) {
+            airportNamesList.add(airport.getFormattedName())
+        }
+        airportListNamesLiveData.value = airportNamesList
+    }
+    fun requestFlightsList(isArrival: Boolean, selectedAirportIndex: Int){
+        viewModelScope.launch {
+
+                val url =  if (isArrival) "https://opensky-network.org/api/flights/arrival" else "https://opensky-network.org/api/flights/departure"
+            val params = HashMap<String, String>()
+            params.put("begin", (beginDateLiveData.value!!.timeInMillis!! / 1000).toString())
+            params.put("end", (endDateLiveData.value!!.timeInMillis!! / 1000).toString())
+
+
+            params.put("airport", airportListLiveData.value!![selectedAirportIndex].icao)
+
+
+            val result = withContext(Dispatchers.IO) {
+                RequestManager.getSuspended(url, params)
+            }
+            if (result != null) {
+                Log.i("REQUEST", result)
+
+            } else {
+                Log.e("REQUEST", "ERROR NO RESULT")
+            }
+
+        }
     }
 
-    fun getAirportList(): LiveData<List<Airport>> {
-        return airportListData
+    enum class DateType {
+        BEGIN, END
+    }
+
+    fun getBeginDateLiveData(): LiveData<Calendar>{
+        return beginDateLiveData
+    }
+
+    fun getEndDateLiveData(): LiveData<Calendar>{
+        return endDateLiveData
+    }
+
+    fun updateCalendarLiveData(dateType: DateType, calendar: Calendar){
+        if(dateType == DateType.BEGIN) beginDateLiveData.value = calendar else endDateLiveData.value = calendar
+    }
+
+    fun getAirportNamesListLiveData():LiveData<List<String>>{
+        return airportListNamesLiveData
+    }
+
+    fun getAirportListLiveData():LiveData<List<Airport>>{
+        return airportListLiveData
     }
 }
 
--- app/src/main/java/com/example/airtrafficmonitoring/activiter/Home.kt
+++ app/src/main/java/com/example/airtrafficmonitoring/activiter/Home.kt
@@ -1,71 +1,102 @@
 package com.example.airtrafficmonitoring.activiter
 
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.Switch
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.airtrafficmonitoring.Airport
 import com.example.airtrafficmonitoring.R
+import com.example.airtrafficmonitoring.RequestManager
 import com.example.airtrafficmonitoring.Utils
 import com.example.airtrafficmonitoring.ViewModels.HomeViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.withContext
 
 class Home : AppCompatActivity() {
+    private val viewModelScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var viewModel:  HomeViewModel
+
+    private lateinit var beginDateLabel: TextView
+    private lateinit var endDateLabel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        Log.i("SERGIO","ici")
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        // Generate the airport list
-        val airportList = Utils.generateAirportList()
-        val spinner = findViewById<Spinner>(R.id.airport_spinner)
-        val adapter = ArrayAdapter<Airport>(this, android.R.layout.simple_spinner_dropdown_item, airportList)
-        spinner.adapter = adapter
-//
-//        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-//
-//
-//
-//        // Set the airport list in the ViewModel()
-//        homeViewModel.setAirportList(airportList)
-//
-//        // Observe changes to the airport list in the ViewModel
-//        homeViewModel.getAirportList().observe(this) { updatedAirportList ->
-//            // Update the adapter with the new data
-//            adapter.clear()
-//            adapter.addAll(updatedAirportList)
-//        }
+        beginDateLabel = findViewById<TextView>(R.id.from_date)
+        endDateLabel = findViewById<TextView>(R.id.to_date)
+
+        beginDateLabel.setOnClickListener { showDatePickerDialog(HomeViewModel.DateType.BEGIN) }
+        endDateLabel.setOnClickListener { showDatePickerDialog(HomeViewModel.DateType.END) }
+
+        val airportSpinner = findViewById<Spinner>(R.id.airport_spinner)
+
+        viewModel.getAirportNamesListLiveData().observe(this) {
+            val adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item, it
+            )
+
+            airportSpinner.adapter = adapter
+        }
+
+
+        viewModel.getBeginDateLiveData().observe(this) {
+            beginDateLabel.text = Utils.dateToString(it.time)
+        }
+
+        viewModel.getEndDateLiveData().observe(this) {
+            endDateLabel.text = Utils.dateToString(it.time)
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            // Récupérer données pour la requête
+            // Airport
+            val selectedAirportIndex = airportSpinner.selectedItemPosition
+            // Depart ou arrivée
+            val isArrival = findViewById<Switch>(R.id.airport_switch).isChecked
+
+
+            viewModel.requestFlightsList(isArrival, selectedAirportIndex)
+        }
+
+
+    }
+
+    // open date picker dialog
+    private fun showDatePickerDialog(dateType: HomeViewModel.DateType) {
+        // Date Select Listener.
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                val calendar = Calendar.getInstance()
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, monthOfYear)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                viewModel.updateCalendarLiveData(dateType, calendar)
+            }
+
+        val currentCalendar = if (dateType == HomeViewModel.DateType.BEGIN) viewModel.getBeginDateLiveData().value else viewModel.getEndDateLiveData().value
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            dateSetListener,
+            currentCalendar!!.get(Calendar.YEAR),
+            currentCalendar.get(Calendar.MONTH),
+            currentCalendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
 }
-//        findViewById<TextView>(R.id.from_date_view).setOnClickListener {
-//            showDatePickerDialog()
-//        }
-//
-//
-//
-//     fun showDatePickerDialog() {
-//        val dateSetListener = OnDateSetListener {view, year, monthOfYear, dayOfMonth ->
-//            val calendar = Calendar.getInstance()
-//            calendar.set(Calendar.YEAR, year)
-//            calendar.set(Calendar.MONTH, monthOfYear)
-//            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-//            findViewById<TextView>(R.id.from_date_view).text= Utils.dateToString(calendar.time)
-//        }
-//
-//        val calendar = Calendar.getInstance()
-//        val datePickerDialog = DatePickerDialog(this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
-//         datePickerDialog.show()
-//
-//        }
-//
-//    }
-
-//
-//
-//    }
-//}
