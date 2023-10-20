dfde8e04f0909d945752cc2b0a7a2ea439ae17cf
oliveur83 <tomolivierpro@gmail.com>
2023-10-20 08:42:20 +0200

    .

--- app/src/main/java/com/example/airtrafficmonitoring/activiter/Home.kt
+++ app/src/main/java/com/example/airtrafficmonitoring/activiter/Home.kt
@@ -1,11 +1,16 @@
 package com.example.airtrafficmonitoring.activiter
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import com.example.airtrafficmonitoring.R
 import com.example.airtrafficmonitoring.Utils
+
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,25 @@ class Home : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item , airportList)
         spinner.adapter = adapter
 
+        val button = findViewById<Button>(R.id.buttons)
+
+        button.setOnClickListener(View.OnClickListener { // Créez une intention (Intent) pour ouvrir la nouvelle activité
+        //   val aeroportDepart = editTextDepart.text.toString()
+            //  val aeroportArrivee = editTextArrivee.text.toString()
+            // val dateDepart = editTextDateDepart.text.toString()
+            // val dateArrivee = editTextDateArrivee.text.toString()
+
+            // Créez un Intent pour passer les données à la nouvelle activité
+            // val intent = Intent(this, ListeResult::class.java)
+
+            // Ajoutez les données en extra dans l'Intent
+            // intent.putExtra("AeroportDepart", aeroportDepart)
+            // intent.putExtra("AeroportArrivee", aeroportArrivee)
+            // intent.putExtra("DateDepart", dateDepart)
+            // intent.putExtra("DateArrivee", dateArrivee)
+            // Démarrez la nouvelle activité
+            startActivity(intent)
+        })
 //        findViewById<TextView>(R.id.from_date_view).setOnClickListener {
 //            showDatePickerDialog()
 //        }
